5ac66adac247f4c934c982908fa7c232fd43d1d5
Sharath M <sharathm5684@gmail.com>
2024-07-26 20:15:44 +0530

    Add dark/light mode toggle, 404 error pages and implement CRUD operations

--- src/components/pages/AddContact.jsx
+++ src/components/pages/AddContact.jsx
@@ -1,7 +1,149 @@
 import "../../assets/style/addcontact.css";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useState } from "react";
+import Loading from "../../../utils/Loading";
+import { addDoc, collection } from "firebase/firestore";
+import { db, storage } from "../../firebase/config";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+
+const schema = z.object({
+  photo: z.instanceof(FileList).optional(),
+  name: z.string().min(3, { message: "Name must have at least 3 characters" }),
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email({ message: "Invalid email address" }),
+  phone: z.string().min(10, { message: "Phone Number must have 10 digits" }),
+});
 
 const AddContact = () => {
-  return <></>;
+  const [imagePreviewURL, setImagePreviewURL] = useState(null);
+  const [error_msgs, setError_msgs] = useState(null);
+
+  const userDetails = useSelector((state) => state.authSlice.userDetails);
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm({ resolver: zodResolver(schema) });
+
+  const onImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const previewURL = URL.createObjectURL(file);
+      setImagePreviewURL(previewURL);
+    }
+  };
+
+  const onSubmit = async (data) => {
+    try {
+      let imageURL = "";
+      if (data.photo && data.photo[0]) {
+        const imageRef = ref(
+          storage,
+          `image/${userDetails.uid}/profile_picture/${data.photo[0].name}`
+        );
+        const snapshot = await uploadBytes(imageRef, data.photo[0]);
+        imageURL = await getDownloadURL(snapshot.ref);
+      }
+
+      await addDoc(collection(db, `users/${userDetails.uid}/contacts/`), {
+        imageURL: imageURL || "",
+        name: data.name,
+        email: data.email,
+        phone: data.phone,
+      });
+
+      navigate("/allcontacts");
+      reset();
+    } catch (error) {
+      setError_msgs(error.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="form_container container">
+        <h1>Add Contact</h1>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="form">
+          {imagePreviewURL ? (
+            <div className="image_preview">
+              <img
+                src={imagePreviewURL}
+                alt="Image Preview"
+                className="image_upload_preview"
+              />
+            </div>
+          ) : (
+            <div className="image_preview">
+              <i
+                className="bi bi-person-circle"
+                style={{ fontSize: "2.8em", color: "var(--text-color)" }}
+              ></i>
+            </div>
+          )}
+
+          <div className="field">
+            <label htmlFor="profile_pic" className="input_label">
+              Choose Profile Photo
+            </label>
+            <input
+              type="file"
+              accept="image/*"
+              className="input_field"
+              {...register("photo")}
+              onChange={onImageChange}
+            />
+          </div>
+
+          <div className="field">
+            <label htmlFor="name" className="input_label">
+              Name
+            </label>
+            <input type="text" className="input_field" {...register("name")} />
+            {errors.name && <p className="errors_msg">{errors.name.message}</p>}
+          </div>
+
+          <div className="field">
+            <label htmlFor="email" className="input_label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="input_field"
+              {...register("email")}
+            />
+            {errors.email && (
+              <p className="errors_msg">{errors.email.message}</p>
+            )}
+          </div>
+
+          <div className="field">
+            <label htmlFor="text" className="input_label">
+              Phone Number
+            </label>
+            <input type="text" className="input_field" {...register("phone")} />
+            {errors.phone && (
+              <p className="errors_msg">{errors.phone.message}</p>
+            )}
+          </div>
+
+          <button type="submit" className="btn" disabled={isSubmitting}>
+            {isSubmitting ? <Loading /> : "Save"}
+          </button>
+          {error_msgs && <p className="errors_msg">{error_msgs}</p>}
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default AddContact;
--- src/components/pages/AllContacts.jsx
+++ src/components/pages/AllContacts.jsx
@@ -1,161 +1,63 @@
-import { useState } from "react";
-import Contact from "../pages/Contact";
+import { useState, useEffect } from "react";
 import "../../assets/style/allcontact.css";
 import SearchBar from "../SearchBar";
 import PageLoading from "../../../utils/PageLoading";
 import Pagination from "../Pagination";
+import { collection, getDocs, query, deleteDoc, doc } from "firebase/firestore";
+import { db, storage } from "../../firebase/config";
+import { useSelector } from "react-redux";
+import { deleteObject, ref } from "firebase/storage";
+import { useNavigate } from "react-router-dom";
 
 const AllContacts = () => {
-  const [fetchedContact, setFetchedContact] = useState([
-    { id: 1, name: "Alex", email: "alex@alex.com", phone: "9876543210" },
-    {
-      id: 2,
-      name: "John Doe",
-      email: "john.doe@example.com",
-      phone: "1234567890",
-    },
-    {
-      id: 3,
-      name: "Jane Smith",
-      email: "jane.smith@example.com",
-      phone: "2345678901",
-    },
-    {
-      id: 4,
-      name: "Michael Brown",
-      email: "michael.brown@example.com",
-      phone: "3456789012",
-    },
-    {
-      id: 5,
-      name: "Emily Johnson",
-      email: "emily.johnson@example.com",
-      phone: "4567890123",
-    },
-    {
-      id: 6,
-      name: "Chris Lee",
-      email: "chris.lee@example.com",
-      phone: "5678901234",
-    },
-    {
-      id: 7,
-      name: "Anna White",
-      email: "anna.white@example.com",
-      phone: "6789012345",
-    },
-    {
-      id: 8,
-      name: "David Wilson",
-      email: "david.wilson@example.com",
-      phone: "7890123456",
-    },
-    {
-      id: 9,
-      name: "Emma Davis",
-      email: "emma.davis@example.com",
-      phone: "8901234567",
-    },
-    {
-      id: 10,
-      name: "Sophia Martinez",
-      email: "sophia.martinez@example.com",
-      phone: "9012345678",
-    },
-    {
-      id: 11,
-      name: "Liam Thompson",
-      email: "liam.thompson@example.com",
-      phone: "1112233445",
-    },
-    {
-      id: 12,
-      name: "Olivia Harris",
-      email: "olivia.harris@example.com",
-      phone: "2223344556",
-    },
-    {
-      id: 13,
-      name: "Noah Lewis",
-      email: "noah.lewis@example.com",
-      phone: "3334455667",
-    },
-    {
-      id: 14,
-      name: "Ava Walker",
-      email: "ava.walker@example.com",
-      phone: "4445566778",
-    },
-    {
-      id: 15,
-      name: "Mason Hall",
-      email: "mason.hall@example.com",
-      phone: "5556677889",
-    },
-    {
-      id: 16,
-      name: "Isabella Young",
-      email: "isabella.young@example.com",
-      phone: "6667788990",
-    },
-    {
-      id: 17,
-      name: "Lucas Allen",
-      email: "lucas.allen@example.com",
-      phone: "7778899001",
-    },
-    {
-      id: 18,
-      name: "Mia King",
-      email: "mia.king@example.com",
-      phone: "8889900112",
-    },
-    {
-      id: 19,
-      name: "Elijah Wright",
-      email: "elijah.wright@example.com",
-      phone: "9990011223",
-    },
-    {
-      id: 20,
-      name: "Amelia Scott",
-      email: "amelia.scott@example.com",
-      phone: "0001122334",
-    },
-    {
-      id: 21,
-      name: "James Green",
-      email: "james.green@example.com",
-      phone: "1231231234",
-    },
-    {
-      id: 22,
-      name: "Charlotte Adams",
-      email: "charlotte.adams@example.com",
-      phone: "2342342345",
-    },
-    {
-      id: 23,
-      name: "Benjamin Baker",
-      email: "benjamin.baker@example.com",
-      phone: "3453453456",
-    },
-    {
-      id: 24,
-      name: "Harper Nelson",
-      email: "harper.nelson@example.com",
-      phone: "4564564567",
-    },
-    {
-      id: 25,
-      name: "Henry Carter",
-      email: "henry.carter@example.com",
-      phone: "5675675678",
-    },
-  ]);
+  const [fetchedContact, setFetchedContact] = useState([]);
   const [contactList, setContactList] = useState([]);
   const [loading, setLoading] = useState(false);
   const [contacts, setContacts] = useState([]);
+  const userDetails = useSelector((state) => state.authSlice.userDetails);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        setLoading(true);
+        const contactSnapshot = await getDocs(
+          query(collection(db, `users/${userDetails.uid}/contacts`))
+        );
+        const fetchedContacts = contactSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setFetchedContact(fetchedContacts);
+      } catch (error) {
+        console.log("all contacts error: ", error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchContacts();
+  }, [userDetails]);
+
+  const handleDeleteContact = async (id, imageURL) => {
+    try {
+      await deleteDoc(doc(db, `users/${userDetails.uid}/contacts/${id}`));
+
+      if (imageURL !== "") {
+        const imageRef = ref(storage, imageURL);
+        await deleteObject(imageRef);
+      }
+
+      setContacts((prev) =>
+        prev.filter((prevContact) => prevContact.id !== id)
+      );
+    } catch (error) {
+      console.log("contact delete error", error.message);
+    }
+  };
+
+  const handleEditContact = (contact) => {
+    navigate("/addcontact", { state: contact });
+  };
 
   return (
     <>
@@ -186,12 +88,55 @@ const AllContacts = () => {
               <tbody>
                 {contacts &&
                   contacts.map((contact) => (
-                    <Contact
-                      key={contact.id}
-                      name={contact.name}
-                      email={contact.email}
-                      phone={contact.phone}
-                    />
+                    <tr className="table_row" key={contact.id}>
+                      <td className="name-column">
+                        <div className="photo_name">
+                          {contact.imageURL === "" ? (
+                            <i
+                              className="bi bi-person-circle"
+                              style={{
+                                fontSize: "2.1em",
+                                color: "var(--text-color)",
+                              }}
+                            ></i>
+                          ) : (
+                            <img
+                              src={contact.imageURL}
+                              className="photo"
+                              alt=""
+                            />
+                          )}
+                          <p>{contact.name}</p>
+                        </div>
+                      </td>
+                      <td className="email-column">{contact.email}</td>
+                      <td className="phone-column">{contact.phone}</td>
+                      <td className="action-column">
+                        <i
+                          className="bi bi-pencil-square"
+                          style={{
+                            fontSize: "20px",
+                            color: "var(--text-color)",
+                            padding: "0 10px",
+                          }}
+                          onClick={() => {
+                            handleEditContact(contact);
+                          }}
+                        ></i>
+
+                        <i
+                          className="bi bi-trash3"
+                          style={{
+                            fontSize: "20px",
+                            color: "var(--text-color)",
+                            padding: "0 10px",
+                          }}
+                          onClick={() => {
+                            handleDeleteContact(contact.id, contact.imageURL);
+                          }}
+                        ></i>
+                      </td>
+                    </tr>
                   ))}
               </tbody>
             </table>
