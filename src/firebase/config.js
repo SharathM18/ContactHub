9585e0201e360f682fb69e526dddaa27876da7e4
Sharath M <sharathm5684@gmail.com>
2024-09-15 13:16:23 +0530

    adjustments and improvements across projecs

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: String(import.meta.env.VITE_API_KEY),
-  authDomain: String(import.meta.env.VITE_AUTH_DOMAIN),
-  projectId: String(import.meta.env.VITE_PROJECT_ID),
-  storageBucket: String(import.meta.env.VITE_STORAGE_BUCKET),
-  messagingSenderId: String(import.meta.env.VITE_MESSAGING_SENDER_ID),
-  appId: String(import.meta.env.VITE_APP_ID),
+  apiKey: "AIzaSyD8OyM_JvHAu28pyU5BY3hBt98xVg-zTqw",
+  authDomain: "contacthub-787df.firebaseapp.com",
+  projectId: "contacthub-787df",
+  storageBucket: "contacthub-787df.appspot.com",
+  messagingSenderId: "147467627312",
+  appId: "1:147467627312:web:e10c0bd95501558ab5f6f5",
 };
 
 // Initialize Firebase
